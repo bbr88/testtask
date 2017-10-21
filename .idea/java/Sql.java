f23c9934c841a786a721b3ece195480d75740ea5
igor.bisarnov <igor.bisarnov@rtlabs.ru>
2017-10-21 14:52:19 +0300

    qqq

--- .idea/java/Sql.java
+++ .idea/java/Sql.java
@@ -4,5 +4,5 @@ public class Sql {
     (SELECT count(userId) as users
     INNER JOIN BRANCH ON COMPANY.companyId = BRANCH.companyId
     INNER JOIN USERS ON BRANCH.branchId = USERS.branchId
-    where company.companyId = c);
+    where company.companyId = c) as users;
 }
